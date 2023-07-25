c70388b078da2aa47c9961a83b9e0f24f0c5ff26
azhar <alih9215@gmail.com>
2023-07-25 17:25:20 +0530

    i did some addition in script

--- calculator.sh
+++ calculator.sh
@@ -24,7 +24,7 @@ case $3 in
    4)
         echo "I'll divide $no1 & $no2"
 	div=$(( $1 % $2 )) 
-            echo "result is $div"
+            echo "result is $div it may be correct or not"
         ;;
    *)   echo "invalid inputes"
 	;;
