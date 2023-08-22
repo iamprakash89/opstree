8e2d52f211edf77e4ef6fac0c438e8d2fd73eea9
Prakash Ashokan <122374176+iamprakash89@users.noreply.github.com>
2023-08-23 00:26:39 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-no1=$1
-no2=$2
-operation=$3
-
-case $3 in
-   1)
-	      echo "I'll add $no1 & $no2"
-        add
-        ;;
-   2)
-        echo "I'll substract $no1 & $no2"
-	      sub
-        ;;
-   3)
-        echo "I'll multiply $no1 & $no2"
-	      mul
-        ;;   
-   4)
-        echo "I'll divide $no1 & $no2"
-	      div
-        ;;
-   *)   echo "invalid inputes"
-        echo "post build actions"
-	echo "Ne lined added for testing"
-	;;
-
-esac
+echo "This is for the test branch"
